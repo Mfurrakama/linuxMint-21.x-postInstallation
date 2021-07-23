26939abd4f2bb6e3f7f40c43db4b4448c234f406
Mfurrakama <50051943+Mfurrakama@users.noreply.github.com>
2021-07-23 17:52:53 -0300

    Update linuxMint-20.x-postInstallation.sh

--- linuxMint-20.x-postInstallation.sh
+++ linuxMint-20.x-postInstallation.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
 ## Removing unnecessary programs | Removendo programas desnecessários ##
-sudo apt remove -y libreoffice-common hexchat thunderbird transmission
+apt remove -y libreoffice-common hexchat thunderbird transmission
 
 ## Ensuring 32-bit support | Garantindo o suporte à 32-bit ##
-sudo dpkg --add-architecture i386
+dpkg --add-architecture i386
 
 ## Installing repositories | Instalando repositórios ##
-sudo add-apt-repository ppa:papirus/papirus -y
-sudo add-apt-repository ppa:lutris-team/lutris -y
-sudo apt-add-repository ppa:graphics-drivers/ppa -y
-sudo add-apt-repository ppa:snwh/ppa -y
+add-apt-repository ppa:papirus/papirus -y
+add-apt-repository ppa:lutris-team/lutris -y
+apt-add-repository ppa:graphics-drivers/ppa -y
+add-apt-repository ppa:snwh/ppa -y
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key
-sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y
+apt-key add winehq.key
+add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y
 
 ## Installing updates, packages, apps and icons | Instalando atualizações, pacotes, apps e ícones ##
-sudo rm /etc/apt/preferences.d/nosnap.pref
-sudo apt update
-sudo apt install -y git libvirt-daemon-system libvirt-clients qemu-kvm qemu-utils virt-manager ovmf qbittorrent snapd neofetch paper-icon-theme papirus-icon-theme mangohud gamemode lutris steam minecraft-launcher --install-recommends winehq-devel
-sudo ubuntu-drivers autoinstall
+rm /etc/apt/preferences.d/nosnap.pref
+apt update
+apt install -y git libvirt-daemon-system libvirt-clients qemu-kvm qemu-utils virt-manager ovmf qbittorrent snapd neofetch paper-icon-theme papirus-icon-theme mangohud gamemode lutris steam minecraft-launcher --install-recommends winehq-devel
+ubuntu-drivers autoinstall
 cd ~/.icons && git clone https://github.com/keeferrourke/la-capitaine-icon-theme.git
 cd ~/Downloads/ && wget -c -O Adapta-Nokto.zip https://cinnamon-spices.linuxmint.com/files/themes/Adapta-Nokto.zip?time=1626796848
 unzip Adapta-Nokto.zip -d ~/.themes && rm Adapta-Nokto.zip
-sudo apt autoclean && sudo apt autoremove -y
-sudo apt dist-upgrade -y
+apt autoclean && sudo apt autoremove -y
+apt dist-upgrade -y
 
 ## Changing the icon, themes, etc. | Trocando os ícones, temas, etc.
 gsettings set org.cinnammon.desktop.wm.preferences theme 'Adapta-Nokto'
@@ -37,7 +37,7 @@ gsettings set org.cinnamon.theme name 'Adapta-Nokto'
 wget -c https://github.com/fireice-uk/xmr-stak/releases/download/1.0.5-rx/xmr-stak-rx-linux-1.0.5-cpu.tar.xz
 tar -xf xmr-stak-rx-linux-1.0.5-cpu.tar.xz && rm xmr-stak-rx-linux-1.0.5-cpu.tar.xz
 chmod +x ~/Downloads/xmr-stak-rx-linux-1.0.5-cpu/xmr-stak-rx
-sudo echo 'vm.nr_hugepages=128' >> /etc/sysctl.conf && sudo sysctl -p
+echo 'vm.nr_hugepages=128' >> /etc/sysctl.conf && sudo sysctl -p
 
 ## Restarting to apply all the upgrades and to Snaps working correctly | Reiniciando para aplicar todas as atualições e para Snaps funcionarem corretamente ##
-sudo reboot now
+reboot now

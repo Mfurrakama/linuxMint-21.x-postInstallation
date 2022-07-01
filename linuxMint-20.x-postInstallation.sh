b7611b27ccfea9d4e3b9a8685d2c23a7cbbeb558
Marcos Furrakama <marcosfurrakama@protonmail.com>
2022-07-01 09:22:32 -0300

    commit on 'main'

--- linuxMint-20.x-postInstallation.sh
+++ linuxMint-20.x-postInstallation.sh
@@ -12,8 +12,6 @@ sudo add-apt-repository ppa:papirus/papirus -y
 sudo add-apt-repository ppa:lutris-team/lutris -y
 sudo add-apt-repository ppa:snwh/ppa -y
 sudo add-apt-repository ppa:flexiondotorg/mangohud -y
-echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
-wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
 sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y
@@ -21,12 +19,19 @@ sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal mai
 # Installing updates, packages, apps and icons. | Instalando atualizações, pacotes, apps e ícones.
 sudo rm /etc/apt/preferences.d/nosnap.pref
 sudo apt update && sudo apt -y full-upgrade
-sudo apt install -y git adb fastboot libvirt-daemon-system libvirt-clients qemu-kvm qemu-utils virt-manager ovmf qbittorrent neofetch paper-icon-theme papirus-icon-theme linux-xanmod-edge neofetch gamemode mangohud lutris steam minecraft-launcher winehq-staging
-flatpak install flathub -y com.discordapp.Discord com.github.Eloston.UngoogledChromium org.DolphinEmu.dolphin-emu org.citra_emu.citra org.signal.Signal com.obsproject.Studio
-cd ~/.icons && git clone https://github.com/keeferrourke/la-capitaine-icon-theme.git
-wget -c -O ~/Downloads/Adapta-Nokto.zip https://cinnamon-spices.linuxmint.com/files/themes/Adapta-Nokto.zip?time=1626796848
-unzip Adapta-Nokto.zip -d ~/.themes && rm Adapta-Nokto.zip
+sudo apt install -y git adb fastboot virt-manager qbittorrent neofetch paper-icon-theme papirus-icon-theme neofetch gamemode mangohud lutris winehq-staging autoconf automake inkscape libgdk-pixbuf2.0-dev libglib2.0-dev libxml2-utils pkg-config sassc parallel v4l2loopback-dkms
 sudo apt autoclean && apt autoremove -y
+
+# Installing some apps via flatpak
+flatpak install flathub -y com.discordapp.Discord com.github.Eloston.UngoogledChromium org.signal.Signal com.obsproject.Studio com.valvesoftware.Steam sh.cider.Cider
+cd ~/.icons && git clone https://github.com/keeferrourke/la-capitaine-icon-theme.git
+
+# Installing Adapta Nokto and Orchis theme
+https://github.com/adapta-project/adapta-gtk-theme "$HOME/adapta-gtk-theme/"
+cd "$HOME/adapta-gtk-theme/"
+./autogen.sh --prefix=/usr --enable-parallel 
+make
+sudo make install
 
 # Changing the icon, themes, etc. | Trocando os ícones, temas, etc.
 gsettings set org.cinnamon.desktop.wm.preferences theme 'Adapta-Nokto'
@@ -35,10 +40,6 @@ gsettings set org.cinnamon.desktop.interface gtk-theme 'Adapta-Nokto'
 gsettings set org.cinnamon.desktop.interface cursor-theme 'paper'
 gsettings set org.cinnamon.theme name 'Adapta-Nokto'
 
-# Downloading xmr-stak-rx and doing its corrections. | Baixando xmr-stak-rx e fazendo suas correções.
-wget -c -O ~/Downloads/xmr-stak-rx-linux-1.0.5-cpu.tar.xz https://github.com/fireice-uk/xmr-stak/releases/download/1.0.5-rx/xmr-stak-rx-linux-1.0.5-cpu.tar.xz
-tar -xf ~/Downloads/xmr-stak-rx-linux-1.0.5-cpu.tar.xz && rm ~/Downloads/xmr-stak-rx-linux-1.0.5-cpu.tar.xz
-sudo chmod +x ~/Downloads/xmr-stak-rx-linux-1.0.5-cpu/xmr-stak-rx
-sudo /bin/su -c "echo 'vm.nr_hugepages=128' >> /etc/sysctl.conf" && sysctl -p
+# It's over!
 neofetch
-echo "Reboot is recommended. | Reinicar é recomendado."
+echo "Your system is ready. | Seu sistema está protno."
